f0dd7453d270e209549609227dd36117311efaf4
Melanie <melanie.beduer@gmail.com>
2021-10-11 14:07:06 +0200

    Organisation fichiers

--- Reconnaissance_vehicules.py
+++ Reconnaissance_vehicules.py
@@ -6,30 +6,41 @@ Created on Thu Oct  7 10:54:16 2021
 """
 
 import cv2 
-import numpy as np 
-   
-cap = cv2.VideoCapture('tree.mp4') 
-   
-if (cap.isOpened()== False):  
-  print("Error opening video  file") 
-   
-while(cap.isOpened()): 
-      
-  
-  ret, frame = cap.read() 
-  if ret == True: 
-   
+
+#Fichier pour la détection des voitures
+cascade_src = 'cars.xml'
+car_cascade = cv2.CascadeClassifier(cascade_src)
+
+#Chargement de la vidéo
+video_src = 'video2.avi'
+cap = cv2.VideoCapture(video_src)
+
+
+while True:
+    #La fonction "read" retourne :
+        #- ret : True/False : permet de détecter la fin de la vidéo
+        #- img : Image de la vidéo
+    ret, img = cap.read()
+    if ret==False:
+        break
     
-    cv2.imshow('Frame', frame) 
-   
+    #Passage de la vidéo en niveau de gris pour faciliter la détection
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    cars = car_cascade.detectMultiScale(gray, 1.1, 1)
+
+    #Dessin des triangles autour des voitures
+    for (x,y,w,h) in cars:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)      
     
-    if cv2.waitKey(25) & 0xFF == ord('q'): 
-      break
-   
-  
-  else:  
-    break
-   
+    #Affichage de la vidéo
+    cv2.imshow('vidéo', img)
+    
+    #Interruption de la vidéo et fermeture avant sa fin
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+#Permet la fermeture automatique de la vidéo à la fin de son visionnage
 cap.release() 
+cv2.destroyAllWindows()
    
-cv2.destroyAllWindows() 
+
